34c5f73ef19614188d8c857d5a8cc1f431d28a26
Darkhhh <a.a.mikov@mail.ru>
2024-03-05 19:09:18 +0300

    Add new Demo.

---
+++ Runner/Demo/Demo6.cs
@@ -0,0 +1,25 @@
+﻿using ResearchConfiguration.Configuration;
+using ResearchConfiguration.Extensions;
+
+namespace Runner.Demo;
+
+public static partial class Demo
+{
+    public static void Demo6()
+    {
+        var reader = Loader.Load("Test", "Configs", getFromProjectDirectory: true);
+
+        var simpleString = reader.Get("MaxStateTime");
+
+        var simpleInt = reader.GetAsInt("MaxStateTime");
+
+        var simpleDouble = reader.GetAsDouble("Nodes", "ObserveRange");
+
+        var simpleEnum = reader.GetAsEnum<AreaTypeEnum>("Area", "Type");
+    }
+
+    public enum AreaTypeEnum
+    {
+        Rectangle, Circle, Triangle
+    }
+}
